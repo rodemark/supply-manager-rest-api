8d022cc32a2925d7f78748488316e786cfc9534d
rodemark <rode.mark01@gmail.com>
2025-03-20 17:19:56 +0300

    initial commit.

---
+++ src/test/java/team/rode/severstalintershiprestapi/SeverstalIntershipRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package team.rode.severstalintershiprestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeverstalIntershipRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
